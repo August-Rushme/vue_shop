5a311a1aebb4f44e869ac84b9f44c369af8269b3
“Augsut-Rushme” <864011713@qq.com>
2021-05-29 21:26:55 +0800

    完成商品功能开发！

--- src/main.js
+++ src/main.js
@@ -9,6 +9,12 @@ import './assets/css/global.css'
 import TreeTable from 'vue-table-with-tree-grid'
 
 import axios from 'axios'
+//导入富文本
+import VueQuillEditor from 'vue-quill-editor'
+
+import 'quill/dist/quill.core.css' // import styles
+import 'quill/dist/quill.snow.css' // for snow theme
+import 'quill/dist/quill.bubble.css' // for bubble theme
 // 配置请求的跟路径
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
 //配置请求拦截器
@@ -18,7 +24,19 @@ axios.interceptors.request.use(config => {
   return config
 })
 Vue.prototype.$http = axios
+Vue.use(VueQuillEditor)
+//时间过滤器
+Vue.filter('dateFormat', function(originVal) {
+  const dt = new Date(originVal)
+  const y = dt.getFullYear()
+  const m = (dt.getMonth() + 1 + '').padStart(2, '0')
+  const d = (dt.getDate() + '').padStart(2, '0')
 
+  const hh = (dt.getHours() + '').padStart(2, '0')
+  const mm = (dt.getMinutes() + '').padStart(2, '0')
+  const ss = (dt.getSeconds() + '').padStart(2, '0')
+  return `${y}-${m}-${d} ${hh}:${mm}:${ss}`
+})
 Vue.config.productionTip = false
 Vue.component('tree-table', TreeTable)
 Vue.prototype.$http = axios
